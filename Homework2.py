17d37ebf76c6aad95d965e22fd91edf3d215bd18
Megan <117634588+MSoD1@users.noreply.github.com>
2024-12-17 13:58:03 +0000

    Update Homework2.py

--- Homework2.py
+++ Homework2.py
@@ -1,15 +1,15 @@
 city = input("Enter the name of a city: ")
-temp = int(input("Enter the temperature in "+ city +": "))
+temperature = input(f"Enter the temperature in {city}: ")
 
-if temp > 20:
-    print(f"It is currently warm in {city} with a temperature of 20ºC")
-elif temp > 10:
-    print(f"It is currently chilly in {city} with a temperature of 15ºC")
-elif temp < 10:
-    print(f"It is currently cold in {city} with a temperature of 5ºC")
-else :
-    #if city == None or str(temp) == None:
-    print("Please try again and enter a city and temperature")
-
- 
+if city and temperature:
+  temperature = int(temperature)
+  
+  if temperature > 20:
+    print(f"It is currently warm in {city} with a temperature of {temperature} degrees.")
+  elif temperature > 10:
+    print(f"It is currently chilly in {city} with a temperature of {temperature} degrees.")
+  else:
+    print(f"It is currently cold in {city} with a temperature of {temperature} degrees.")
+else:
+  print("Please try again and enter a city and temperature")
     
