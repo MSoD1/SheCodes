bec4459648383aa9bb90eb2f5fbce4d34f69a9a7
Megan <117634588+MSoD1@users.noreply.github.com>
2024-12-17 11:26:54 +0000

    Add files via upload

---
+++ Week2Lesson3.py
@@ -0,0 +1,15 @@
+#Create a new Repl
+#Ask what the current temperature is
+#If the temperature is more than 20, output:
+#Enjoy a sunny day
+#If the temperature is less than 20, output:
+#Don’t forget your jacket
+
+
+temp = int(input("What is the current temperature? "))
+
+if temp > 20:
+    print("Enjoy the sunny day")
+elif temp < 20:
+    print("Don’t forget your jacket")
+
